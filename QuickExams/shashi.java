23b14be93f364147eca7745114bebc4c0eee25fc
abhi0661236 <ap0661236@gmail.com>
2021-03-24 21:49:29 +0530

    Some more practice

--- QuickExams/shashi.java
+++ QuickExams/shashi.java
@@ -1,4 +1,15 @@
-import java.util.Scanner;
-public class shashi{
-    public static void main(String [] a)
+public class Shashi{
+    public static void main(String[]args){
+        int a[][] = new a[][];
+        int i = 0;
+        int j = 0;
+        while (i<=4){
+            while (j <= 4){
+                a[i][j] = i*j;
+                System.out.println(a[i][j]);
+                j++;
+            }
+            i++;
+        }
+    }
 }
